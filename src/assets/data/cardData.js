3a351e4ae3a20338995be9c71a50d318b50d65bc
Jeff <jeffrey.celone@gmail.com>
2023-02-07 16:03:16 -0500

    Update portfolio card layout and styles

--- src/assets/data/cardData.js
+++ src/assets/data/cardData.js
@@ -8,28 +8,28 @@ export const cardData = [
         id: 1,
         img: pokePic,
         title: 'PokeAPI PokeDex',
-        build: ['react', 'MUI', 'pokeAPI'],
+        build: ['React', 'MUI'],
         description: 'Fully responsive website built to consume the free pokeAPI. Used Material UI for styling and components. Page allows the user to query the API for informtion about pokemon and berries, and returns that information in a clean and concise UI.'
     },
     {
         id: 2,
         img: rpsPic,
         title: 'Rock, Paper, Scissors',
-        build: ['react', 'styled components'],
+        build: ['React', 'Styled Components'],
         description: 'Web app to play a game of Rock, Paper, Scissors agianst a computer opponent. The app keeps a running score which is updated based on the outcome of each round. Possible outcomes are win/draw/lose.'
     },
     {
         id: 3,
         img: countriesPic,
         title: 'RestCountriesAPI',
-        build: ['react', 'styled components', 'rest countries API'],
+        build: ['React', 'Styled Components'],
         description: 'Informational site built consuming the free rest countries API. Allows the user to search based on country name, and click on each card for more information about each country.'
     },
     {
         id: 4,
         img: listingsPic,
         title: 'Job Listings Page',
-        build: ['react', 'styled components'],
+        build: ['React', 'Styled Components'],
         description: 'Sample single page layout of a job listings site. Clicking on filter tabs allows the user to filter the listings. Page was built to the design of a mockup provided.'
     },
 ]
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,18 +2,17 @@ import styled from "styled-components";
 import { useState } from "react";
 import { ArrowRightSquareFill, ArrowLeftSquareFill } from "styled-icons/bootstrap";
 
-
-// background: ${props => props.theme.primary};
-// background-image: url(${props => props.bgimg});
-// backgound-position: center;
-// background-size: contain;
-// background-repeat: no-repeat;
-// backface-visibility: hidden;
-const Container = styled.article`
+const Container = styled.div`
     margin: auto;
     height: 300px;
     position: relative;
     width: 400px;
+`;
+
+const Row = styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
 `;
 
 const Card = styled.div`
@@ -33,28 +32,21 @@ const CardFront = styled.div`
     overflow: hidden;
     border-radius: 10px;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: start;
+    box-shadow: 0 0 5px ${props => props.theme.secondary};
     background: ${props => props.theme.primary};
+`;
+
+const CardBack = styled(CardFront)`
+    justify-content: start;
+    backface-visibility: hidden;
+    transform: rotateY(180deg);
+    padding: 20px;
 `;
 
 const CardImg = styled.img`
     width: 400px;
     height: auto;
-`;
-
-const CardBack = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 20px;
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    border-radius: 10px;
-    position: absolute;
-    backface-visibility: hidden;
-    transform: rotateY(180deg);
-    background: ${props => props.theme.primary};
 `;
 
 const Title = styled.h3`
@@ -63,28 +55,36 @@ const Title = styled.h3`
 `;
 
 const FrontTitle = styled(Title)`
-    margin-left: 20px;
+    margin: 10px 20px 25px;
 `;
 
 const Text = styled.p`
     font-size: ${props => props.size === 'small' ? '12px' : '16px'};
-    margin-top: 10px;
+    margin: 10px 5px;
 `;
 
 const LinkContainer = styled.div`
     display: flex;
     width: 90%;
-    justify-content: start;
+    justify-content: ${props => props.start || 'end'};
+    position: absolute;
+    bottom: 10px;
 `;
 
 const LinkButton = styled.button`
     width: 50px;
-    height: 30px;
+    height: 25px;
     border: none;
-    border-radius: 10px;
-    background: ${props => props.theme.dark};
+    border-radius: 5px;
+    background: ${props => props.theme.color};
+    color: ${props => props.theme.background};
     margin: auto 15px;
     cursor: pointer;
+
+    &:hover {
+        transition: background 0.5s;
+        background: ${props => props.theme.tertiary};
+    }
 `;
 
 const RightIcon = styled(ArrowRightSquareFill)`
@@ -92,7 +92,7 @@ const RightIcon = styled(ArrowRightSquareFill)`
     position: absolute;
     right: 5px;
     bottom: 5px;
-    color: white;
+    cursor: pointer;
 `;
 
 const LeftIcon = styled(ArrowLeftSquareFill)`
@@ -100,6 +100,7 @@ const LeftIcon = styled(ArrowLeftSquareFill)`
     position: absolute;
     bottom: 5px;
     left: 5px;
+    cursor: pointer;
 `;
 
 // will take a project object as props
@@ -118,19 +119,21 @@ const ProjectCard = ({ project }) => {
                     <CardImg src={img} alt='project' />
                     <FrontTitle>{title}</FrontTitle>
                     <RightIcon onClick={() => flipCard()} />
-                    <LinkContainer>
+                    <LinkContainer start='start'>
                         <LinkButton>live</LinkButton>
                         <LinkButton>code</LinkButton>
                     </LinkContainer>
                 </CardFront>
                 <CardBack>
                     <Title>{title}</Title>
-                    <Text size='small'>
-                        built with:
-                    </Text>
-                    {build.map((b) => {
-                        return <span><Text size='small'>{b}</Text></span>
-                    })}
+                    <Row>
+                        <Text size='small'>
+                            built with:
+                        </Text>
+                        {build.map((b) => {
+                            return <Text size='small' key={b}>{b}</Text>
+                        })}
+                    </Row>
                     <Text>{description}</Text>
                     <LinkContainer>
                         <LinkButton>live</LinkButton>
